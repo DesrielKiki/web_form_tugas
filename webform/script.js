894b85ddf43d62fe2d1675f3387a3297efe72b2c
Desrielkiki <desriel7c6@gmail.com>
2024-10-20 18:46:12 +0700

    fix show password bug & add contact

--- webform/script.js
+++ webform/script.js
@@ -2,27 +2,18 @@ function submitLoginForm() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
   var gender = document.querySelector('input[name="gender"]:checked');
-  var education = document.querySelectorAll(
-    'input[name="education[]"]:checked'
-  );
+  var education = document.querySelectorAll('input[name="education[]"]:checked');
   var province = document.getElementById("province").value;
   var fileInput = document.getElementById("file").value;
 
-  if (
-    email === "" ||
-    password === "" ||
-    !gender ||
-    education.length === 0 ||
-    province === "" ||
-    fileInput === ""
-  ) {
-    alert("Please fill all the fields");
-    return false;
+  if (email === "" || password === "" || !gender || education.length === 0 || province === "" || fileInput === "") {
+      alert("Please fill all the fields");
+      return false;
   }
 
   if (!validateEmail(email)) {
-    alert("Please enter a valid email address");
-    return false;
+      alert("Please enter a valid email address");
+      return false;
   }
 
   alert("Login successful!");
@@ -35,10 +26,7 @@ function validateEmail(email) {
 }
 
 function togglePassword() {
-  var passwordField = document.getElementById("password");
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-  } else {
-    passwordField.type = "password";
-  }
+  const passwordInput = document.getElementById('password');
+  const showPasswordCheckbox = document.getElementById('show-password');
+  passwordInput.type = showPasswordCheckbox.checked ? 'text' : 'password';
 }
